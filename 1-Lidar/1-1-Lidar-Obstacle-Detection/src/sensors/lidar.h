957b24cc036103f1aaf736e344268a904fd51fa1
jonathanloganmoran <jonathan.moran107@gmail.com>
2024-04-22 19:59:32 -0700

    :art: Enforcing Google C++ Style Guide

--- 1-Lidar/1-1-Lidar-Obstacle-Detection/src/sensors/lidar.h
+++ 1-Lidar/1-1-Lidar-Obstacle-Detection/src/sensors/lidar.h
@@ -11,6 +11,7 @@
 #ifndef LIDAR_H
 #define LIDAR_H
 
+
 #include "../render/render.h"
 #include <ctime>
 #include <chrono>
@@ -37,60 +38,62 @@ struct Ray {
 	Vect3 direction;
 	Vect3 castPosition;
 	double castDistance;
-
-	// parameters:
-	// setOrigin: the starting position from where the ray is cast
-	// horizontalAngle: the angle of direction the ray travels on the xy plane
-	// verticalAngle: the angle of direction between xy plane and ray 
-	// 				  for example 0 radians is along xy plane and pi/2 radians is stright up
-	// resoultion: the magnitude of the ray's step, used for ray casting, the smaller the more accurate but the more expensive
-
-	Ray(Vect3 setOrigin, double horizontalAngle, double verticalAngle, double setResolution)
-		: origin(setOrigin), resolution(setResolution), direction(resolution*cos(verticalAngle)*cos(horizontalAngle), resolution*cos(verticalAngle)*sin(horizontalAngle),resolution*sin(verticalAngle)),
-		  castPosition(origin), castDistance(0)
-	{}
-
+	Ray(Vect3 setOrigin, 
+		double horizontalAngle, 
+		double verticalAngle, 
+		double setResolution
+	) : origin(setOrigin), 
+		resolution(setResolution),
+		direction(
+			resolution * cos(verticalAngle) * cos(horizontalAngle), 
+			resolution * cos(verticalAngle) * sin(horizontalAngle),
+			resolution * sin(verticalAngle)
+		),
+		castPosition(origin),
+		castDistance(0) {}
 	void rayCast(
-		const std::vector<Car>& cars, double minDistance, double maxDistance, pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, double slopeAngle, double sderr)
-	{
+		const std::vector<Car>& cars, 
+		double minDistance, 
+		double maxDistance, 
+		pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, 
+		double slopeAngle, 
+		double sderr
+	) {
 		// reset ray
 		castPosition = origin;
 		castDistance = 0;
-
 		bool collision = false;
-
-		while(!collision && castDistance < maxDistance)
-		{
-
+		while (!collision && castDistance < maxDistance) {
 			castPosition = castPosition + direction;
 			castDistance += resolution;
-
 			// check if there is any collisions with ground slope
 			collision = (castPosition.z <= castPosition.x * tan(slopeAngle));
-
 			// check if there is any collisions with cars
-			if(!collision && castDistance < maxDistance)
-			{
-				for(Car car : cars)
-				{
+			if (!collision && castDistance < maxDistance) {
+				for (Car car : cars) {
 					collision |= car.checkCollision(castPosition);
-					if(collision)
+					if (collision) {
 						break;
+					}
 				}
 			}
 		}
-
-		if((castDistance >= minDistance)&&(castDistance<=maxDistance))
-		{
+		if (castDistance >= minDistance
+			&& castDistance <= maxDistance
+		) {
 			// add noise based on standard deviation error
 			double rx = ((double) rand() / (RAND_MAX));
 			double ry = ((double) rand() / (RAND_MAX));
 			double rz = ((double) rand() / (RAND_MAX));
-			cloud->points.push_back(pcl::PointXYZ(castPosition.x+rx*sderr, castPosition.y+ry*sderr, castPosition.z+rz*sderr));
+			cloud->points.push_back(
+				pcl::PointXYZ(
+					castPosition.x + rx * sderr, 
+					castPosition.y + ry * sderr, 
+					castPosition.z + rz * sderr
+				)
+			);
 		}
-			
 	}
-
 };
 
 
@@ -110,9 +113,7 @@ struct Ray {
  * @func	pcl::PointCloud<pcl::PointXYZ>::Ptr		scan
  * @brief	Generates a point cloud scan of the environment.
  * */
-struct Lidar
-{
-
+struct Lidar {
 	std::vector<Ray> rays;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
 	std::vector<Car> cars;
@@ -123,9 +124,11 @@ struct Lidar
 	double resoultion;
 	double sderr;
 
-	Lidar(std::vector<Car> setCars, double setGroundSlope)
-		: cloud(new pcl::PointCloud<pcl::PointXYZ>()), position(0,0,2.6)
-	{
+	Lidar(
+		std::vector<Car> setCars, 
+		double setGroundSlope
+	) : cloud(new pcl::PointCloud<pcl::PointXYZ>()),
+		position(0, 0, 2.6) {
 		resoultion = 0.2;
 		cars = setCars;
 		groundSlope = setGroundSlope;
@@ -133,50 +136,58 @@ struct Lidar
         /** E1.1.4: Increase the vertical scanning resolution. **/
 		// Modifying the number of vertical beams to increase resolution
         int numLayers = 8;                      	// Number of vertically-stacked diodes
-		// the steepest vertical angle
-		double steepestAngle =  30.0*(-pi/180);
-		double angleRange = 26.0*(pi/180);
 		/** E1.1.5: Increase the horizontal scanning resolution. **/
         // Modifying the angular "spacing" to improve resolution
         double horizontalAngleInc = pi / 64;     	// Radians
 		/** E1.1.6: "Remove" the points reflected off the ego-vehicle. **/
         // Discarding all points at distances below `minDistance`
-        minDistance = 0;
 		minDistance = 5;                        	// Distance given in metres (m)
 		maxDistance = 50;
 		/** E1.1.7: Add "noise" to the sensor measurements. **/
         // Modelling measurement uncertainty with non-zero value
         sderr = 0.0;                            	// Standard deviation of error
-		double angleIncrement = angleRange/numLayers;
-		for(double angleVertical = steepestAngle; angleVertical < steepestAngle+angleRange; angleVertical+=angleIncrement)
-		{
-			for(double angle = 0; angle <= 2*pi; angle+=horizontalAngleInc)
-			{
-				Ray ray(position,angle,angleVertical,resoultion);
+		/* Other LiDAR parameters */
+		// Steepest vertical angle
+		double steepestAngle =  30.0 * (-pi / 180);
+		double angleRange = 26.0 * (pi / 180);
+		double angleIncrement = angleRange / numLayers;
+		for (double angleVertical = steepestAngle; angleVertical < steepestAngle + angleRange; angleVertical += angleIncrement) {
+			for (double angle = 0; angle <= 2 * pi; angle += horizontalAngleInc) {
+				Ray ray(position,
+						angle,
+						angleVertical,
+						resoultion
+				);
 				rays.push_back(ray);
 			}
 		}
 	}
-
-	~Lidar()
-	{
-		// pcl uses boost smart pointers for cloud pointer so we don't have to worry about manually freeing the memory
+	~Lidar() {
+		// NOTE: PCL uses `boost` smart pointers for cloud pointer, 
+		// So we don't have to worry about manually freeing the memory.
 	}
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr scan()
-	{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr scan() {
 		cloud->points.clear();
 		auto startTime = std::chrono::steady_clock::now();
 		for(Ray ray : rays)
-			ray.rayCast(cars, minDistance, maxDistance, cloud, groundSlope, sderr);
+			ray.rayCast(cars, 
+						minDistance, 
+						maxDistance, 
+						cloud, 
+						groundSlope, 
+						sderr
+		);
 		auto endTime = std::chrono::steady_clock::now();
-		auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+		auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+			endTime - startTime
+		);
 		cout << "ray casting took " << elapsedTime.count() << " milliseconds" << endl;
 		cloud->width = cloud->points.size();
 		cloud->height = 1; // one dimensional unorganized point cloud dataset
 		return cloud;
 	}
-
 };
+
 
 #endif
