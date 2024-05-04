3e687c7c3e0dc15b4d0865575ace45ecd8c47bff
jonathanloganmoran <jonathan.moran107@gmail.com>
2024-05-04 15:11:15 -0700

    :bulb: Adding comments

--- 1-Lidar/1-1-Lidar-Obstacle-Detection/src/processPointClouds.h
+++ 1-Lidar/1-1-Lidar-Obstacle-Detection/src/processPointClouds.h
@@ -42,8 +42,10 @@ public:
 
     typename pcl::PointCloud<PointT>::Ptr FilterCloud(typename pcl::PointCloud<PointT>::Ptr cloud, float filterRes, Eigen::Vector4f minPoint, Eigen::Vector4f maxPoint);
 
+    // Extracts the plane and obstacles using the Point Cloud Library (PCL).
     std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SeparateClouds(pcl::PointIndices::Ptr inliers, typename pcl::PointCloud<PointT>::Ptr cloud);
 
+    // Segments the input cloud into two using the Point Cloud Library (PCL).
     std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SegmentPlane(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold);
 
     std::vector<typename pcl::PointCloud<PointT>::Ptr> Clustering(typename pcl::PointCloud<PointT>::Ptr cloud, float clusterTolerance, int minSize, int maxSize);
