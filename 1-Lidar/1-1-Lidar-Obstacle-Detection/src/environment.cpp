5eb0a6c99ee40937b3a183a5abd492621960f830
Jonathan Logan Moran <jonathan.moran107@gmail.com>
2024-07-12 13:06:30 -0700

    :alembic: Performing E1.4.2(a) experiment run — Defining region of interest In this first experiment run, we begin by defining the `minPoint` / `maxPoint` values which have been manually-selected to constrain the total input point cloud to points determined to lie within an area of visibility we are interested in. These values have been provided by Udacity (i.e., the learning content generators).

--- 1-Lidar/1-1-Lidar-Obstacle-Detection/src/environment.cpp
+++ 1-Lidar/1-1-Lidar-Obstacle-Detection/src/environment.cpp
@@ -112,8 +112,8 @@ void cityBlock(
     >::Ptr regionCloud = pointProcessorI->FilterCloud(
         inputCloud,
         0.2f,
-        Eigen::Vector4f(),
-        Eigen::Vector4f()
+        Eigen::Vector4f(-1.5, -1.7, -1. 1),
+        Eigen::Vector4f(2.6, 1.7, -0.4, 1)
     );
     std::cerr << "Loaded " << filterCloud->points.size() 
             << " data points from filtered cloud\n";
